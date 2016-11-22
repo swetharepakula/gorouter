93fe573dbff0a53ed172e05b4b584e2b917fcff3
Chris Piraino <cpiraino@pivotal.io>
2016-11-21 16:17:28 -0800

    Remove registry code to test performance
    Signed-off-by: Shash Reddy <sreddy@pivotal.io>

--- registry/registry.go
+++ registry/registry.go
@@ -11,6 +11,7 @@ import (
 	"code.cloudfoundry.org/gorouter/registry/container"
 	"code.cloudfoundry.org/gorouter/route"
 	"code.cloudfoundry.org/lager"
+	"code.cloudfoundry.org/routing-api/models"
 )
 
 //go:generate counterfeiter -o fakes/fake_registry_interface.go . RegistryInterface
@@ -52,6 +53,7 @@ type RouteRegistry struct {
 
 	ticker           *time.Ticker
 	timeOfLastUpdate time.Time
+	pool             *route.Pool
 }
 
 func NewRouteRegistry(logger lager.Logger, c *config.Config, reporter reporter.RouteRegistryReporter) *RouteRegistry {
@@ -63,190 +65,52 @@ func NewRouteRegistry(logger lager.Logger, c *config.Config, reporter reporter.R
 	r.dropletStaleThreshold = c.DropletStaleThreshold
 	r.suspendPruning = func() bool { return false }
 
+	pool := route.NewPool(10*time.Second, "")
+	endpoint := route.NewEndpoint("", "gostatic-0.foo.com", 8080, "", "", nil, 120, "", models.ModificationTag{})
+	pool.Put(endpoint)
+	r.pool = pool
+
 	r.reporter = reporter
 	return r
 }
 
 func (r *RouteRegistry) Register(uri route.Uri, endpoint *route.Endpoint) {
-	t := time.Now()
-	data := lager.Data{"uri": uri, "backend": endpoint.CanonicalAddr(), "modification_tag": endpoint.ModificationTag}
-
-	r.reporter.CaptureRegistryMessage(endpoint)
-
-	r.Lock()
-
-	uri = uri.RouteKey()
-
-	pool := r.byUri.Find(uri)
-	if pool == nil {
-		contextPath := parseContextPath(uri)
-		pool = route.NewPool(r.dropletStaleThreshold/4, contextPath)
-		r.byUri.Insert(uri, pool)
-		r.logger.Debug("uri-added", lager.Data{"uri": uri})
-	}
-
-	endpointAdded := pool.Put(endpoint)
-
-	r.timeOfLastUpdate = t
-	r.Unlock()
-
-	if endpointAdded {
-		r.logger.Debug("endpoint-registered", data)
-	} else {
-		r.logger.Debug("endpoint-not-registered", data)
-	}
 }
 
 func (r *RouteRegistry) Unregister(uri route.Uri, endpoint *route.Endpoint) {
-	data := lager.Data{"uri": uri, "backend": endpoint.CanonicalAddr(), "modification_tag": endpoint.ModificationTag}
-	r.reporter.CaptureRegistryMessage(endpoint)
-
-	r.Lock()
-
-	uri = uri.RouteKey()
-
-	pool := r.byUri.Find(uri)
-	if pool != nil {
-		endpointRemoved := pool.Remove(endpoint)
-		if endpointRemoved {
-			r.logger.Debug("endpoint-unregistered", data)
-		} else {
-			r.logger.Debug("endpoint-not-unregistered", data)
-		}
-
-		if pool.IsEmpty() {
-			r.byUri.Delete(uri)
-		}
-	}
-
-	r.Unlock()
 }
 
 func (r *RouteRegistry) Lookup(uri route.Uri) *route.Pool {
-	started := time.Now()
-
-	r.RLock()
-
-	uri = uri.RouteKey()
-	var err error
-	pool := r.byUri.MatchUri(uri)
-	for pool == nil && err == nil {
-		uri, err = uri.NextWildcard()
-		pool = r.byUri.MatchUri(uri)
-	}
-
-	r.RUnlock()
-	endLookup := time.Now()
-	r.reporter.CaptureLookupTime(endLookup.Sub(started))
-	return pool
+	return r.pool
 }
 
 func (r *RouteRegistry) LookupWithInstance(uri route.Uri, appId string, appIndex string) *route.Pool {
-	uri = uri.RouteKey()
-	p := r.Lookup(uri)
-
-	var surgicalPool *route.Pool
-
-	p.Each(func(e *route.Endpoint) {
-		if (e.ApplicationId == appId) && (e.PrivateInstanceIndex == appIndex) {
-			surgicalPool = route.NewPool(0, "")
-			surgicalPool.Put(e)
-		}
-	})
-	return surgicalPool
+	return r.Lookup(uri)
 }
 
 func (r *RouteRegistry) StartPruningCycle() {
-	if r.pruneStaleDropletsInterval > 0 {
-		r.Lock()
-		r.ticker = time.NewTicker(r.pruneStaleDropletsInterval)
-		r.Unlock()
-
-		go func() {
-			for {
-				select {
-				case <-r.ticker.C:
-					r.logger.Info("start-pruning-droplets")
-					r.pruneStaleDroplets()
-					r.logger.Info("finished-pruning-droplets")
-					msSinceLastUpdate := uint64(time.Since(r.TimeOfLastUpdate()) / time.Millisecond)
-					r.reporter.CaptureRouteStats(r.NumUris(), msSinceLastUpdate)
-				}
-			}
-		}()
-	}
 }
 
 func (r *RouteRegistry) StopPruningCycle() {
-	r.Lock()
-	if r.ticker != nil {
-		r.ticker.Stop()
-	}
-	r.Unlock()
 }
 
 func (registry *RouteRegistry) NumUris() int {
-	registry.RLock()
-	uriCount := registry.byUri.PoolCount()
-	registry.RUnlock()
-
-	return uriCount
+	return 1
 }
 
 func (r *RouteRegistry) TimeOfLastUpdate() time.Time {
-	r.RLock()
-	t := r.timeOfLastUpdate
-	r.RUnlock()
-
-	return t
+	return time.Now()
 }
 
 func (r *RouteRegistry) NumEndpoints() int {
-	r.RLock()
-	count := r.byUri.EndpointCount()
-	r.RUnlock()
-
-	return count
+	return 1
 }
 
 func (r *RouteRegistry) MarshalJSON() ([]byte, error) {
-	r.RLock()
-	defer r.RUnlock()
-
 	return json.Marshal(r.byUri.ToMap())
 }
 
 func (r *RouteRegistry) pruneStaleDroplets() {
-	r.Lock()
-	defer r.Unlock()
-
-	// suspend pruning if option enabled and if NATS is unavailable
-	if r.suspendPruning() {
-		r.logger.Info("prune-suspended")
-		r.pruningStatus = DISCONNECTED
-		return
-	} else {
-		if r.pruningStatus == DISCONNECTED {
-			// if we are coming back from being disconnected from source,
-			// bulk update routes / mark updated to avoid pruning right away
-			r.logger.Debug("prune-unsuspended-refresh-routes-start")
-			r.freshenRoutes()
-			r.logger.Debug("prune-unsuspended-refresh-routes-complete")
-		}
-		r.pruningStatus = CONNECTED
-	}
-
-	r.byUri.EachNodeWithPool(func(t *container.Trie) {
-		endpoints := t.Pool.PruneEndpoints(r.dropletStaleThreshold)
-		t.Snip()
-		if len(endpoints) > 0 {
-			addresses := []string{}
-			for _, e := range endpoints {
-				addresses = append(addresses, e.CanonicalAddr())
-			}
-			r.logger.Info("pruned-route", lager.Data{"uri": t.ToPath(), "endpoints": addresses})
-		}
-	})
 }
 
 func (r *RouteRegistry) SuspendPruning(f func() bool) {
@@ -257,10 +121,6 @@ func (r *RouteRegistry) SuspendPruning(f func() bool) {
 
 // bulk update to mark pool / endpoints as updated
 func (r *RouteRegistry) freshenRoutes() {
-	now := time.Now()
-	r.byUri.EachNodeWithPool(func(t *container.Trie) {
-		t.Pool.MarkUpdated(now)
-	})
 }
 
 func parseContextPath(uri route.Uri) string {
